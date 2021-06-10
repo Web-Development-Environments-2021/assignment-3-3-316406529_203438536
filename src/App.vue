fa584c2140f307ee4f27feecacb427b4ae6202a0
noam <kotlar@post.bgu.ac.il>
2021-06-11 00:20:19 +0300

    off aouto complete

--- src/App.vue
+++ src/App.vue
@@ -85,7 +85,7 @@ export default {
     }
   },
   mounted() {
-    this.setSearchAutoCompleteData();
+    // this.setSearchAutoCompleteData();
     this.$root.store.setStoredData("","","","","");
   },
 };
