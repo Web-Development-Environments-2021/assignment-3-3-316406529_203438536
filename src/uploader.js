76864535f34408e9a8eb74cf67c439a2f2019500
noam <kotlar@post.bgu.ac.il>
2021-06-14 18:38:27 +0300

    upload media

---
+++ src/uploader.js
@@ -0,0 +1,47 @@
+const Cloud = require('@google-cloud/storage')
+const path = require('path')
+const serviceKey = path.join(__dirname, './keys.json')
+
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+async function uploadFile(filePath) {
+    await storage.bucket(noambk).upload(filePath, {
+      destination: destFileName,
+    });
+    console.log(`${filePath} uploaded to ${bucketName}`);
+}
+uploadFile().catch(console.error);
+exports.uploadFile= uploadFile;
+// module.exports = storage
+
+// const util = require('util')
+// const gc = require('./config/')
+// const bucket = gc.bucket('all-mighti') // should be your bucket name
+
+// /**
+//  *
+//  * @param { File } object file object that will be uploaded
+//  * @description - This function does the following
+//  * - It uploads a file to the image bucket on Google Cloud
+//  * - It accepts an object as an argument with the
+//  *   "originalname" and "buffer" as keys
+//  */
+
+// export const uploadImage = (file) => new Promise((resolve, reject) => {
+//   const { originalname, buffer } = file
+
+//   const blob = bucket.file(originalname.replace(/ /g, "_"))
+//   const blobStream = blob.createWriteStream({
+//     resumable: false
+//   })
+//   blobStream.on('finish', () => {
+//     const publicUrl = format(
+//       `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+//     )
+//     resolve(publicUrl)
+//   })
+//   .on('error', () => {
+//     reject(`Unable to upload image, something went wrong`)
+//   })
+//   .end(buffer)
+// })
